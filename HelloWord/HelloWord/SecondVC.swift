c8df0154ca0483c73c03ba766c9a58f3c13c5a9c
Usuário Convidado <Guest@PA06MICRO14.local>
2017-08-21 22:48:29 -0300

    helloword

---
+++ HelloWord/HelloWord/SecondVC.swift
@@ -0,0 +1,35 @@
+//
+//  SecondVC.swift
+//  HelloWord
+//
+//  Created by Usuário Convidado on 21/08/17.
+//  Copyright © 2017 Paty Alves. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
