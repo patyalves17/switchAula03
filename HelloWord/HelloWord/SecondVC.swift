905fb73b1c4a679256e9981908e647e750ddefe4
Usuário Convidado <Guest@PA06MICRO14.local>
2017-08-21 23:05:50 -0300

    helloword

--- HelloWord/HelloWord/SecondVC.swift
+++ HelloWord/HelloWord/SecondVC.swift
@@ -12,16 +12,36 @@ class SecondVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print("Tela 2: viewDidLoad")
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        print("Tela 2: didReceiveMemoryWarning")
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Tela 2: viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Tela 2: viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("Tela 2: viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("Tela 2: viewDidDisappear")
+    }
     /*
     // MARK: - Navigation
 
--- HelloWord/HelloWord/ViewController.swift
+++ HelloWord/HelloWord/ViewController.swift
@@ -12,30 +12,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+         print("Tela 1: viewDidLoad")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        print("Tela 1: didReceiveMemoryWarning")
         // Dispose of any resources that can be recreated.
     }
 
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        print("Tela 1: viewWillAppear")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        print("Tela 1: viewDidAppear")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        print("Tela 1: viewWillDisappear")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
+        print("Tela 1: viewDidDisappear")
     }
+    
+    
 
 }
 
