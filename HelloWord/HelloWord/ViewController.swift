43b8a467daa11b5cecfbe8532f8f5eceffdbfbba
Usuário Convidado <Guest@PA06MICRO14.local>
2017-08-21 22:56:57 -0300

    helloword

--- HelloWord/HelloWord/ViewController.swift
+++ HelloWord/HelloWord/ViewController.swift
@@ -20,6 +20,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
 
 }
 
